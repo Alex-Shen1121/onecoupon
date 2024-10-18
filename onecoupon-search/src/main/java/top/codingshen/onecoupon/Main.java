0885ea86f6463aedfc78333ba4353ccc64ed9555
shency <alex_shency@outlook.com>
2024-10-19 00:53:44 +0800

    feat: 初始化子模块

---
+++ onecoupon-search/src/main/java/top/codingshen/onecoupon/Main.java
@@ -0,0 +1,13 @@
+package top.codingshen.onecoupon;
+
+/**
+ * @ClassName ${NAME}
+ * @Description description
+ * @Author alex_shen
+ * @Date 2024/10/19 - 00:50
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
